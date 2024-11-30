ebb5d0024fe664f601637e96b6613e9af4cb6132
AskaSlip <varenaska@gmail.com>
2024-11-30 22:50:12 +0400

    updated migration

--- src/database/migrations/1732201555669-add-basic-tables.ts
+++ src/database/migrations/1732201555669-add-basic-tables.ts
@@ -4,14 +4,13 @@ export class AddBasicTables1732201555669 implements MigrationInterface {
     name = 'AddBasicTables1732201555669'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "isActive" boolean NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TYPE "public"."mechanics_role_enum" AS ENUM('guest', 'user', 'manager', 'admin', 'showroom_admin', 'showroom_manager', 'showroom_mechanic')`);
         await queryRunner.query(`CREATE TABLE "mechanics" ("created" TIMESTAMP NOT NULL DEFAULT now(), "updated" TIMESTAMP NOT NULL DEFAULT now(), "id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" text NOT NULL, "email" text NOT NULL, "phone" text NOT NULL, "experience" text, "role" "public"."mechanics_role_enum" NOT NULL DEFAULT 'showroom_mechanic', CONSTRAINT "UQ_301c3b5fa2d49aab65dcfda36a6" UNIQUE ("email"), CONSTRAINT "PK_2c0ed23afc0cc7ff361c17e53df" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "car_showroom" ("created" TIMESTAMP NOT NULL DEFAULT now(), "updated" TIMESTAMP NOT NULL DEFAULT now(), "id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" text NOT NULL, "description" text, "email" text NOT NULL, "phone" text, CONSTRAINT "UQ_b659cf8f334b81785d2f7d04652" UNIQUE ("email"), CONSTRAINT "PK_43d253875381455394b49051fc5" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TYPE "public"."offers_brand_enum" AS ENUM('BMW', 'AUDI', 'MERCEDES', 'VOLKSWAGEN', 'TOYOTA', 'HONDA', 'FORD', 'CHEVROLET', 'NISSAN', 'HYUNDAI', 'KIA', 'MAZDA', 'PEUGEOT', 'CITROEN', 'RENAULT', 'FIAT', 'OPEL', 'VOLVO', 'SKODA', 'SEAT', 'MINI', 'JEEP', 'LAND ROVER', 'PORSHE', 'JAGUAR', 'LEXUS', 'DEAWOO')`);
         await queryRunner.query(`CREATE TYPE "public"."offers_currency_enum" AS ENUM('USD', 'EUR', 'UAH')`);
         await queryRunner.query(`CREATE TABLE "offers" ("created" TIMESTAMP NOT NULL DEFAULT now(), "updated" TIMESTAMP NOT NULL DEFAULT now(), "id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" text NOT NULL, "description" text, "brand" "public"."offers_brand_enum" NOT NULL, "model" text NOT NULL, "year" text, "price" text NOT NULL, "currency" "public"."offers_currency_enum" NOT NULL, "city" text NOT NULL, "region" text NOT NULL, "image" text, "isActive" boolean NOT NULL DEFAULT true, "isSalon" boolean NOT NULL DEFAULT false, CONSTRAINT "PK_4c88e956195bba85977da21b8f4" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "firstName"`);
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "lastName"`);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "isActive"`);
         await queryRunner.query(`ALTER TABLE "users" ADD "created" TIMESTAMP NOT NULL DEFAULT now()`);
         await queryRunner.query(`ALTER TABLE "users" ADD "updated" TIMESTAMP NOT NULL DEFAULT now()`);
@@ -32,6 +31,7 @@ export class AddBasicTables1732201555669 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "users"`);
         await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433"`);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "id"`);
         await queryRunner.query(`ALTER TABLE "users" ADD "id" SERIAL NOT NULL`);
